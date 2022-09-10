939f532032bd797111033924300b78c65d3a9494
MATRIX-feather <midnightcarnival@outlook.com>
2022-09-10 21:03:13 +0800

    fix: 修复插件问题
    Signed-off-by: MATRIX-feather <midnightcarnival@outlook.com>

--- src/main/java/xiamomc/timingplugin/ScoreboardHandler.java
+++ src/main/java/xiamomc/timingplugin/ScoreboardHandler.java
@@ -1,31 +1,40 @@
 package xiamomc.timingplugin;
 
+import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scoreboard.*;
 
 import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
 
 public class ScoreboardHandler {
     ScoreboardManager manager = Bukkit.getScoreboardManager();
-    Scoreboard scb = manager.getNewScoreboard();
-    Objective obj = scb.registerNewObjective(
-            "Time",
-            "dummy",
-            "Time"
-    );
+    Scoreboard scb = manager.getMainScoreboard();
+    Objective obj = scb.getObjective("xiamoTime");
+
+    Instant baseTime;
 
     public ScoreboardHandler(){
-        obj.setDisplaySlot(DisplaySlot.SIDEBAR);
+        Calendar cal = Calendar.getInstance();
+        cal.set(2022, Calendar.JANUARY, 1, 0, 0, 0);
+        baseTime = cal.getTime().toInstant();
+
+        if (scb.getObjective("xiamoTime") == null)
+            obj = scb.registerNewObjective("xiamoTime", "dummy", Component.text("RealTime"));
+
+        //obj.setDisplaySlot(DisplaySlot.SIDEBAR);
         long timestamp = Instant.now().getEpochSecond();
         Score unixTimestamp = obj.getScore("UnixTimestamp");
         unixTimestamp.setScore((int) timestamp);
         new BukkitRunnable() {
             @Override
             public void run() {
-                long timestamp = Instant.now().getEpochSecond();
-                unixTimestamp.setScore((int) timestamp);
+                long timestamp = Instant.now().getEpochSecond() - baseTime.getEpochSecond();
+                unixTimestamp.setScore((int) timestamp * 20);
             }
         }.runTaskTimer(TimingPlugin.instance, 0, 1);
     }
